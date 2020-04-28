4ececfa9b4d3196f5bd2d642506a0816cc2e12fc
Amol <amole.babar@gmail.com>
2020-04-28 14:16:38 +0530

    V6.14

---
+++ MNH Core Server/Server CleanUp Scripts/iqa_server_cleanup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# ps -eo pid,cmd,tty,args | while read PID CMD TTY ARG; do echo $PID $CMD $TTY $ARG; done
+
+
+
+echo "Stopping Jenkins...";
+sudo service jenkins stop
+echo "Stopped Jenkins...";
+
+sleep 2;
+
+ps -eo pid,cmd,tty,args | while read PID CMD TTY ARG;
+do
+
+#echo $PID $CMD $TTY $ARG;
+
+#echo $ARG;
+
+if [[ $ARG == *"/usr/bin/java -Djava.util.logging.config.file=/opt/tomcat/conf/logging.properties"* ]]; then
+  #echo "Tomcat Process Id:$PID";
+  echo "Stopping Tomcat...";
+  sudo kill -9 $PID
+  echo "Stopped Tomcat...";
+fi
+
+if [[ $ARG == *"java -jar /home/ubuntu/CurrentProjects/trunk/SQSCore/target/SQSCore.jar"* ]] && ! [[ $ARG == *"sudo"*  ]] ; then
+  #echo "QR Process Id:$PID";
+  echo "Stopping QR...";
+  sudo kill -9 $PID
+  echo "Stopped QR...";
+
+fi
+done
+
+if [ $1 ]; then
+echo "Cleaning application logs...";
+cd /home/ubuntu/WOWAppLogs
+ls -l
+sudo rm -r *;
+ls -l
+echo "Cleaned application logs...";
+fi
+
+echo "";
+
+if [ $2 ]; then
+echo "Cleaning tomcat logs...";
+cd /opt/tomcat/temp
+ls -l
+sudo rm -r *;
+ls -l
+cd /opt/tomcat/logs
+ls -l
+sudo rm -r *;
+ls -l
+echo "Cleaned tomcat logs...";
+fi
+
+#sleep 10;
+
+echo "Starting Tomcat...";
+sudo service tomcat start
+echo "Started Tomcat...";
+
+echo "Waiting for Tomcat to warm-up[1 minute]";
+for i in {1..120}
+do
+  echo -n -e ".";
+  sleep 0.5
+done
+
+echo ""
+echo "Starting QR...";
+sudo java -jar /home/ubuntu/CurrentProjects/trunk/SQSCore/target/SQSCore.jar & disown
+echo "Started QR...";
+
+
+echo "Starting Jenkins...";
+sudo service jenkins start
+echo "Started Jenkings..."
+
